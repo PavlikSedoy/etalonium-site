a6f81cad9188616071ad632fca6fe266df8b95b9
pavliksedoy@gmail.com <45nhigf8>
2019-03-24 14:21:56 +0200

    Home Component. Auto Change Title Text (Shuffle Effect)

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import './Home.scss'
+
+import Messenger from '../Messenger/Messenger'
 
 const Home = () => {
     return (
         <div>
-            h1.
+            <Messenger news={[
+                'Welcome to Decentrilize Fashion World',
+                'Choose your creatives and promote them',
+                'Become a producer in world of fashion',
+                'Make Dream profitable'
+            ]}/>
         </div>
     )
 }
