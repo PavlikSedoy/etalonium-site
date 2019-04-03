d61ae104d48b149c163f83f3eb6ab57d43855f12
Павел Седой <pavliksedoy@gmail.com>
2019-04-03 14:15:30 +0300

    befor passing state from parent

--- src/Container.js
+++ src/Container.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Switch, Route, withRouter } from 'react-router-dom'
 import { TransitionGroup, CSSTransition } from 'react-transition-group'
 
@@ -8,28 +8,45 @@ import What from "./components/What/What";
 import WhoPreloader from "./components/WhoPreloader/WhoPreloader";
 import Who from "./components/Who/Who";
 
-function Container({ location }) {
-    return (
-        <TransitionGroup>
-            <CSSTransition
-                key={location.key}
-                // transitionEnterTimeout={5000}
-                // transitionLeaveTimeout={3000}
-                timeout={{ enter: 5000, exit: 5000 }}
-                classNames={'fade'}
-            >
-                <section className="route-section">
-                    <Switch location={location}>
-                        <Route exact path={'/'} component={Home} />
-                        <Route path={'/whatpreloader'} component={WhatPreloader} />
-                        <Route path={'/what'} component={What} />
-                        <Route path={'/whopreloader'} component={WhoPreloader} />
-                        <Route path={'/who'} component={Who} />
-                    </Switch>
-                </section>
-            </CSSTransition>
-        </TransitionGroup>
-    )
+
+class Container extends Component {
+
+    state = {
+        whatRedirect: false,
+        whoRedirect: false
+    }
+
+    updateWhatRedirect = () => {
+        this.setState({
+            whatRedirect: !this.state.whatRedirect
+        })
+    }
+
+    render() {
+        const { location } = this.props
+
+        return (
+            <TransitionGroup>
+                <CSSTransition
+                    key={location.key}
+                    // transitionEnterTimeout={5000}
+                    // transitionLeaveTimeout={3000}
+                    timeout={{ enter: 5000, exit: 5000 }}
+                    classNames={'fade'}
+                >
+                    <section className="route-section">
+                        <Switch location={location}>
+                            <Route exact path={'/'} component={Home} />
+                            <Route path={'/whatpreloader'} component={() => <WhatPreloader updateRedirect={this.updateWhatRedirect} /> } />
+                            <Route path={'/what'} component={What} />
+                            <Route path={'/whopreloader'} component={WhoPreloader} />
+                            <Route path={'/who'} component={Who} />
+                        </Switch>
+                    </section>
+                </CSSTransition>
+            </TransitionGroup>
+        )
+    }
 }
 
 export default withRouter(Container)
